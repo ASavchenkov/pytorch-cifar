d4c9afdc2077aa984c7b572d1649575982c14e10
Alex <asavchenkov1@gmail.com>
2018-01-07 00:00:35 -0800

    added preact_simple (not finished)

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,9 @@ from .senet import *
 from .resnet import *
 from .resnext import *
 from .densenet import *
+from .iterative import *
 from .googlenet import *
 from .mobilenet import *
 from .shufflenet import *
 from .preact_resnet import *
+from .preact_simple import *
