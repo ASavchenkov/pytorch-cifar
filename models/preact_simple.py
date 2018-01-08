6cf874ce10526808ad618b31aef4e2018a5d0f13
Alex <asavchenkov1@gmail.com>
2018-01-07 21:59:21 -0800

    working simple resnet. required division

--- models/preact_simple.py
+++ models/preact_simple.py
@@ -65,66 +65,85 @@ class PreActBottleneck(nn.Module):
 
 class PreActSimple(nn.Module):
 
-    def __init__(self, features):
+    def __init__(self, planes,ratio):
         super().__init__()
-        self.bn = nn.BatchNorm2d(features)
-        self.conv = nn.Conv2d(features, features, kernel_size = 3, stride=1, padding=1, bias=False)
+        self.bn = nn.BatchNorm2d(planes)
+        self.conv = nn.Conv2d(planes, planes, kernel_size = 3, stride=1, padding=1, bias=False)
+        self.ratio = ratio
 
     def forward(self, x):
-        return x + self.conv(self.bn(x))
+        return x + self.conv(F.relu(self.bn(x)))/self.ratio
+        # return x + F.relu(self.bn(self.conv(x)))
+
+#one of these at the end of every group
+class Transition(nn.Module):
+    def __init__(self, planes):
+        super().__init__()
+        self.bn = nn.BatchNorm2d(planes)
+        self.conv = nn.Conv2d(planes, planes, kernel_size = 3, stride=1, padding=1, bias=False)
+
+    def forward(self, x):
+        x = torch.cat((x,self.conv(F.relu(self.bn(x)))),dim=1) #double planes without breaking residuality
+        return F.avg_pool2d(x,2)
 
 
 class SimpleResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, num_blocks, num_classes=10):
         super().__init__()
-        self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.group1 = self._make_group(64, num_blocks[0])
+        self.group2 = self._make_group(128, num_blocks[1])
+        self.group3 = self._make_group(256, num_blocks[2])
+        self.group4 = self._make_group(512, num_blocks[3],False)
+        self.linear = nn.Linear(512, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
+    #increases dimensionality and pools without using any parameters
+    #used to avoid having to make weird group convs 
+    def _make_group(self, planes, num_blocks, transition=True):
+
         layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
+        
+        for i in range(num_blocks):
+            layers.append(PreActSimple(planes,num_blocks))
+
+        if transition:
+            layers.append(Transition(planes))
+        
         return nn.Sequential(*layers)
+
 
     def forward(self, x):
         out = self.conv1(x)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
+        out = self.group1(out)
+        out = self.group2(out)
+        out = self.group3(out)
+        out = self.group4(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
 
-def PreActResNet18():
-    return PreActResNet(PreActBlock, [2,2,2,2])
+# def PreActResNet18():
+    # return PreActResNet(PreActBlock, [2,2,2,2])
 
-def PreActResNet34():
-    return PreActResNet(PreActBlock, [3,4,6,3])
+# def PreActResNet34():
+    # return PreActResNet(PreActBlock, [3,4,6,3])
 
-def PreActResNet50():
-    return PreActResNet(PreActBottleneck, [3,4,6,3])
+# def PreActResNet50():
+    # return PreActResNet(PreActBottleneck, [3,4,6,3])
 
-def PreActResNet101():
-    return PreActResNet(PreActBottleneck, [3,4,23,3])
+# def PreActResNet101():
+    # return PreActResNet(PreActBottleneck, [3,4,23,3])
 
-def PreActResNet152():
-    return PreActResNet(PreActBottleneck, [3,8,36,3])
+# def PreActResNet152():
+    # return PreActResNet(PreActBottleneck, [3,8,36,3])
 
 
-def test():
-    net = PreActResNet18()
-    y = net(Variable(torch.randn(1,3,32,32)))
-    print(y.size())
+# def test():
+    # net = PreActResNet18()
+    # y = net(Variable(torch.randn(1,3,32,32)))
+    # print(y.size())
 
 # test()
