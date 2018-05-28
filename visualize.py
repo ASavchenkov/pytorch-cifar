bd52648438d7499f85c7675b639a40195cc4c38a
Alex <asavchenkov1@gmail.com>
2018-05-27 20:40:39 -0700

    added first set of heatmaps

--- visualize.py
+++ visualize.py
@@ -5,19 +5,19 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
-selectors = pickle.load( open( 'l2_l1_reg/030', 'rb' ) )
+selectors = pickle.load( open( 'l2_l1_reg/020', 'rb' ) )
 
 #we throw out the biases because they're not important for pruning
 weights = [s for s in selectors if len(s.shape)==4]
 
 weights = [w.reshape((w.shape[0],w.shape[1])) for w in weights]
 weights = [np.abs(w) for w in weights]
-weights = [w/np.std(w) for w in weights]
+# weights = [w/np.std(w) for w in weights]
 
 max_width = 600
 
 
-weights = [np.pad(w,((0,0),(0,600-w.shape[1])),'constant',constant_values=-1) for w in weights]
+weights = [np.pad(w,((0,0),(0,600-w.shape[1])),'constant',constant_values=-0.01) for w in weights]
 
 weights = np.concatenate(weights,axis=0)
 print(weights.shape)
